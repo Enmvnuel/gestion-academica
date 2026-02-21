a4e4a09c675e8b81a96ff97e96fa045fccbee92f
Cencio236 <sebasrovg16@gmail.com>
2026-02-21 02:36:42 -0500

    Villavicencio: Entrega Bloque 3 - Logos e identidad link d notas

--- src/main/java/com/cibertec/gestionacademica/controller/DashboardController.java
+++ src/main/java/com/cibertec/gestionacademica/controller/DashboardController.java
@@ -1,6 +1,7 @@
 package com.cibertec.gestionacademica.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.servlet.http.HttpSession;
@@ -25,8 +26,23 @@ public class DashboardController {
     }
 
     @GetMapping("/estudiante")
-    public String panelEstudiante(HttpSession session) {
-        if (session.getAttribute("usuarioLogueado") == null) return "redirect:/login";
+    public String panelEstudiante(HttpSession session, Model model) {
+    
+        com.cibertec.gestionacademica.model.Usuario usuario = (com.cibertec.gestionacademica.model.Usuario) session.getAttribute("usuarioLogueado");
+
+        if (usuario == null) return "redirect:/login";
+
+        int estudianteIdReal;
+        
+        if (usuario.getUsername().equals("jperez")) {
+            estudianteIdReal = 1; 
+        } else if (usuario.getUsername().equals("mgomez")) {
+            estudianteIdReal = 2; 
+        } else {
+            estudianteIdReal = usuario.getId(); 
+        }
+
+        model.addAttribute("estudianteId", estudianteIdReal);
         
         return "dashboard/home_estudiante"; 
     }
